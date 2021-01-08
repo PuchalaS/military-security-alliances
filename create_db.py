6a48ff0084f05b124c0fb18e07243c99c50e4de8
Puchala_Sebastian <sebastian.puchala.95@gmail.com>
2021-01-08 15:08:05 +0100

    Created combined db and implemented alliances queries

--- create_db.py
+++ create_db.py
@@ -1,5 +1,5 @@
 import couchdb
-from save_restore_db import dump
+from couchdb.design import ViewDefinition
 
 couch = couchdb.Server("http://admin:admin@localhost:5984")
 
@@ -18,6 +18,12 @@ if 'alliances' in couch:
 else:
     alliances = couch.create('alliances')
 
+if 'combined' in couch:
+    #alliances = couch['alliances']
+    del couch['combined']
+    combined = couch.create('combined')
+else:
+    combined = couch.create('combined')
 
 tanks['1'] = {'Id':"1",'Country':'Afghanistan','Type': 'T-55', 'Quantity': '601', 'Origin': 'Soviet Union'}
 tanks['2'] = {'Id':"2",'Country':'Afghanistan','Type': 'T-62', 'Quantity': '170', 'Origin': 'Soviet Union'}
@@ -28,8 +34,83 @@ tanks['6'] = {'Id':"6",'Country':'Iran','Type': 'M60 Patton', 'Quantity': '150',
 tanks['7'] = {'Id':"7",'Country':'Iran','Type': 'Chieftain (tank) MK3', 'Quantity': '100', 'Origin': 'United Kingdom'}
 tanks['8'] = {'Id':"8",'Country':'Chad','Type': 'T-55', 'Quantity': '60', 'Origin': 'Soviet Union'}
 
-alliances['1'] = {'Id':"1",'Name':'The U.S.–Afghanistan Strategic Partnership Agreement',
-                                                                     'Countries': ['Afghanistan', 'USA'], 'Start':'2004 ','End':'0'}
+alliances['1'] = {'Id':"1",'Name':'The U.S.–Afghanistan Strategic Partnership Agreement','Countries': ['Afghanistan', 'USA'], 'Start':'2004 ','End':'0'}
 alliances['2'] = {'Id':"2",'Name':'Axis of Resistance', 'Countries': ['Iran', 'Syria'], 'Start':'2004 ','End':'0'}
-alliances['3'] = {'Id':"3",'Name':'Twoj stary', 'Countries': ['Iran', 'Syria'], 'Start':'2004 ','End':'0'}
-#dump(database_name="tanks" dump --dump-to=tanks.json")
+alliances['3'] = {'Id':"3",'Name':'random alliance', 'Countries': ['Iran', 'Syria', 'Chad','Afghanistan','Bangladesh'], 'Start':'2004 ','End':'0'}
+
+#Mapping functions
+def documentMapper(doc):
+    if doc.get('Id'):
+        _id = doc['Id']
+        yield(_id, doc)
+
+def threadCountMapper(doc):
+    if doc.get('Country'):
+        _author = doc['Country']
+        yield(_author, 1)
+
+def tank_orgin_mapper(doc):
+    if doc.get('Id'):
+        _country = doc['Country']
+        _orgin = doc['Origin']
+        _quantity = doc['Quantity']
+        yield([_country,_orgin], int(_quantity))
+
+def alliance_orgin_tanks_mapper(doc):
+    if doc.get('Id'):
+        _name = doc['Name']
+        _orgin = doc['Origin']
+        _quantity = doc['Quantity']
+        yield([_name,_orgin], int(_quantity))
+
+#Mapping reducing functions
+
+def summingReducer(keys, values, rereduce):
+    return sum(values)
+
+
+#for row in tanks.view('_all_docs'):
+#    print(row.id)
+
+#View creation
+
+
+
+view = ViewDefinition('index', 'thread_tank_view',documentMapper, language = 'python')
+view.sync(tanks)
+
+#view for country and tank quantity and orgin
+view = ViewDefinition('index', 'tank_orgin_view',tank_orgin_mapper, reduce_fun = summingReducer, language = 'python')
+view.sync(tanks)
+
+
+view = ViewDefinition('index', 'thread_alliance_view',documentMapper, language = 'python')
+view.sync(alliances)
+
+i=1
+for a_vrow in alliances.view('index/thread_alliance_view'):
+    a_row  = a_vrow.value
+    a_countries = a_row.get('Countries')
+    a_alliance = a_row.get('Name')
+    a_start = a_row.get('Start')
+    a_end = a_row.get('End')
+    for country in a_countries:
+        for t_vrow in tanks.view('index/thread_tank_view'): 
+            t_row  = t_vrow.value
+            t_country = t_row.get('Country')
+            if t_country == country:
+                t_type = t_row.get('Type')
+                t_quantity = t_row.get('Quantity')
+                t_orgin = t_row.get('Origin')
+                combined[str(i)] = {'Id':str(i),'Country': t_country,'Type': t_type, 'Quantity': t_quantity, 'Origin': t_orgin,'Name':a_alliance, 'Start':a_start,'End':a_end}
+                print (combined[str(i)] )
+                i+=1
+                
+            
+
+view = ViewDefinition('index', 'thread_combined_view',documentMapper, language = 'python')
+view.sync(combined)
+
+#view for alliance - orgin and of quantity tanks 
+view = ViewDefinition('index', 'alliance_tank_orgin_view',alliance_orgin_tanks_mapper, reduce_fun = summingReducer, language = 'python')
+view.sync(combined)
--- queries_db.py
+++ queries_db.py
@@ -1,58 +1,29 @@
 import couchdb
 from couchdb.design import ViewDefinition
 from prettytable import PrettyTable
+from collections import defaultdict 
+
 
 couch = couchdb.Server("http://admin:admin@localhost:5984")
 
 
 tanks = couch['tanks']
 alliances = couch['alliances']
-
-def documentMapper(doc):
-    if doc.get('Id'):
-        _id = doc['Id']
-        yield(_id, doc)
-
-def threadCountMapper(doc):
-    if doc.get('Country'):
-        _author = doc['Country']
-        yield(_author, 1)
-
-def tank_orgin_mapper(doc):
-    if doc.get('Id'):
-        _country = doc['Country']
-        _orgin = doc['Origin']
-        _quantity = doc['Quantity']
-        yield([_country,_orgin], int(_quantity))
-        
-def summingReducer(keys, values, rereduce):
-    return sum(values)
-
-def summing_from_list_Reducer(keys, values, rereduce):
-
-    return sum(values)
-
-#for row in tanks.view('_all_docs'):
-#    print(row.id)
+combined = couch['combined']
 
 
-view = ViewDefinition('index', 'count_threads', threadCountMapper, reduce_fun = summingReducer, language = 'python')
-view.sync(tanks)
 
-view = ViewDefinition('index', 'thread_tank_view',documentMapper, language = 'python')
-view.sync(tanks)
-
-view = ViewDefinition('index', 'thread_alliance_view',documentMapper, language = 'python')
-view.sync(alliances)
-
-#widok z krajem iloscia czolgow z danego zrodla
-view = ViewDefinition('index', 'tank_orgin_view',tank_orgin_mapper, reduce_fun = summing_from_list_Reducer, language = 'python')
-view.sync(tanks)
 
 for row in tanks.view('index/tank_orgin_view', group_level = 2):
     print(str(row.key) + " " + str(row.value))
 
+
+#
+#Queries
+#
 def country_tank_info(x_country):
+    '''Query for country - getting tanks information
+    returns: tuple list of (tank_type, quantity)'''
     tank_quantity_list = []
     for vrow in tanks.view('index/thread_tank_view'):
         row  = vrow.value
@@ -62,6 +33,8 @@ def country_tank_info(x_country):
     return tank_quantity_list
 
 def country_aliance_info(x_country):
+    '''Query for country - alliance  information
+    returns: list of country alliances'''
     country_aliance_list = []
     for vrow in alliances.view('index/thread_alliance_view'):
         row  = vrow.value
@@ -70,7 +43,9 @@ def country_aliance_info(x_country):
             country_aliance_list.append(row.get('Name'))
     return country_aliance_list
 
-def country_tank_seller_origin(x_country):
+def country_tank_seller_origin_info(x_country):
+    '''Query for country - tanks orgin information
+    returns: tuple list of (orgin, quantity)'''
     country_tank_seller_origin_list = []
     for vrow in tanks.view('index/tank_orgin_view', group_level = 2):
         row  = vrow.key
@@ -79,15 +54,61 @@ def country_tank_seller_origin(x_country):
             country_tank_seller_origin_list.append((row[1], vrow.value))
     return country_tank_seller_origin_list
 
-country_tank_info = country_tank_info("Iran")
+def alliance_tanks_info(x_alliance):
+    '''Query for alliance - getting tanks information
+    returns: tuple list of (tank_type, quantity)'''
+    alliance_tank_list = []
+    for vrow in alliances.view('index/thread_alliance_view'):
+        row  = vrow.value
+        alliance = row.get('Name')
+        if (alliance == x_alliance):
+            countries = row.get('Countries')
+            for country in countries:
+                country_tank = country_tank_info(country)
+                if country_tank: 
+                    for tank in country_tank:
+                        alliance_tank_list.append(tank)
+            print (alliance_tank_list)
+            output = defaultdict(int) 
+            for k, v in alliance_tank_list:
+                output[k] += int(v)
+    return list(output.items())
+
+def alliance_countries_info(x_alliance):
+    '''Query for alliance - getting member countries information
+    returns: list of alliance members'''
+    for vrow in alliances.view('index/thread_alliance_view'):
+        row  = vrow.value
+        alliance = row.get('Name')
+        if alliance == x_alliance:
+            return row.get('Countries')
+
+def alliance_tanks_origin_info(x_alliance):
+    '''Query for alliance - tanks orgin information
+    returns: tuple list of (orgin, quantity)'''
+    country_tank_seller_origin_list = []
+    for vrow in combined.view('index/alliance_tank_orgin_view', group_level = 2):
+        row  = vrow.key
+        alliance = row[0]
+        if (alliance == x_alliance):
+            country_tank_seller_origin_list.append((row[1], vrow.value))
+    return country_tank_seller_origin_list
+
+
+country_tanks = country_tank_info("Iran")
 country_aliance_info = country_aliance_info("Iran")
-country_tank_seller_origin_info = country_tank_seller_origin("Iran")
+country_tank_seller_origin_info = country_tank_seller_origin_info("Iran")
+alliance_tanks = alliance_tanks_info('random alliance')
+alliance_countries = alliance_countries_info('random alliance')
+alliance_tanks_origin = alliance_tanks_origin_info('random alliance')
 
-
-print(sum(int(n) for _,n in country_tank_info))
-print(country_tank_info) 
+print(sum(int(n) for _,n in country_tanks))
+print(country_tanks) 
 print(country_aliance_info)
 print(country_tank_seller_origin_info)
+print(alliance_tanks)
+print(alliance_countries)
+print(alliance_tanks_origin)
 #for row in tanks.view('index/count_threads', group_level = 1):
 #    print(str(row.key) + " " + str(row.value))
 
